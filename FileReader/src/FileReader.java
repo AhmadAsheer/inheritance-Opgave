35f5db76576631e11d8df99ce32f0a60f89a9b85
ahmad <ahmadasheer@hotmail.com>
2021-04-07 16:10:40 +0200

    Exercise 2

--- FileReader/src/FileReader.java
+++ FileReader/src/FileReader.java
@@ -6,19 +6,25 @@ import java.util.Scanner;
 public class FileReader {
     public static void main(String[] args) throws FileNotFoundException {
 
+        //I create the file reader and i also import it. i locate the file i want java to read
         File file = new File("C:\\Users\\ahmad\\Documents\\Test\\Classified.txt");
+        //I create a scanner so java can read the file
         Scanner scan = new Scanner(file);
+        // i print it out
         System.out.println(scan.nextLine());
 
         System.out.println("");
 
-        try (Scanner sc = new Scanner(new FileInputStream(file))) {
+        //Here i use fileInputStream so java can read the file while also implementing a scanner
+        try (Scanner scan2 = new Scanner(new FileInputStream(file))) {
             int count = 0;
-            while (sc.hasNext()) {
-                sc.next();
+            //Here i use a while loop so i keeps counting until there isn't more words
+            while (scan2.hasNext()) {
+                scan2.next();
                 count++;
 
             }
+            //i print out the amount of words
             System.out.println("Number of words in the file: " + count);
         }
     }
